c36dc65e555ef9c624d72f7b3d22fc9fccf773dd
DavidAshburn <oneinchwebbing@gmail.com>
2022-07-20 23:49:49 -0700

    chore: finish the Dataset class

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,7 +8,21 @@ attendees.clean_values
 
 puts "Event manager Initialized!"
 
-attendees.puts_registration_weekdays
+=begin
+#Module - Event Manager methods
+#display_legislators
+Call this to display the list of attendees and their relevant legislators
+
+#legislator_letters
+Call this to output the .erb template for each attendee to an output folder
+
+#puts_registration_hours
+Call to rank the hours by registrations
+
+#puts_registration_weekdays
+Call to rank the days by registrations
+
+=end
 
 
 
